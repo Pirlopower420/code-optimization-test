afba081c59594d98f43e8c5cb52c10b33c44c473
Pirlopower420 <jhonhamiltongomezmolina22@gmail.com>
2024-09-20 17:47:31 -0500

    mejore algunas partes del codigo que no estaban entendibles

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 public class AgeValidation {
-
-    // Constante para el límite de edad
-    private static final int LIMITE_EDAD = 18;
-
     public static void main(String[] args) {
+        // Constante para el límite de edad
+        final int LIMITE_EDAD = 18;
+
         // Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
 
@@ -15,12 +14,9 @@ public class AgeValidation {
 
         // Validar el acceso basado en la edad ingresada
         if (edad >= LIMITE_EDAD) {
-            System.out.println("Acceso concedido"); // Mensaje si la edad es mayor o igual al límite
+        System.out.println("Acceso concedido"); // Mensaje si la edad es mayor o igual al límite
         } else {
-            System.out.println("Acceso denegado");  // Mensaje si la edad es menor al límite
-        }
-
-        // Cerrar el objeto Scanner para liberar recursos
-        scanner.close();
+         System.out.println("Acceso denegado");  // Mensaje si la edad es menor al límite
+         }        
     }
 }
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -1,11 +1,11 @@
 public class TaxCalculation {
-
-    // Constantes para tasas de impuestos y umbral
-    private static final double TAX_RATE_HIGH = 0.15; // Tasa de impuesto alta
-    private static final double TAX_RATE_LOW = 0.10;  // Tasa de impuesto baja
-    private static final double TAX_THRESHOLD = 50.0; // Umbral para determinar alto o bajo impuesto
-
     public static void main(String[] args) {
+    // Constantes para tasas de impuestos y umbral
+    final double TAX_RATE_HIGH = 0.15; // Tasa de impuesto alta
+    final double TAX_RATE_LOW = 0.10;  // Tasa de impuesto baja
+    final double TAX_THRESHOLD = 50.0; // Umbral para determinar alto o bajo impuesto
+
+
         // Arrays para almacenar los precios de los productos y las tasas de impuestos correspondientes
         double[] precios = {100, 200};
         double[] tasasImpuesto = {TAX_RATE_HIGH, TAX_RATE_LOW};
@@ -21,7 +21,7 @@ public class TaxCalculation {
         if (totalImpuesto > TAX_THRESHOLD) {
             System.out.println("Impuesto total alto: " + totalImpuesto);
         } else {
-            System.out.println("Impuesto bajo");
+            System.out.println("Impuesto bajo: "+ totalImpuesto);
         }
     }
 }
